cd3c1650adce22f10395a3511ddd7d46b2682003
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-16 17:25:19 +0530

    request/res

--- first-app.js
+++ first-app.js
@@ -1,5 +1,33 @@
 const http= require('http');
 const server=http.createServer((req,res)=>{
-    console.log("sumit")
+    const url=req.url;
+    if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>My First page</title></head>');
+    res.write('<body><h1>Hello from node js server</h1></body>')
+    res.write('</html>');
+    res.end();
+    }
+    else if(url=== '/home'){
+        res.write('<html>');
+    res.write('<head><title>My First page</title></head>');
+    res.write('<body><h1>Welcome home</h1></body>')
+    res.write('</html>');
+    res.end();
+    }
+    else if(url=== '/about'){
+        res.write('<html>');
+    res.write('<head><title>My First page</title></head>');
+    res.write('<body><h1>Welcome to about page</h1></body>')
+    res.write('</html>');
+    res.end();
+    }
+    else if(url=== '/node'){
+        res.write('<html>');
+    res.write('<head><title>My First page</title></head>');
+    res.write('<body><h1>Welcome to node js page</h1></body>')
+    res.write('</html>');
+    res.end();
+    }
 })
-server.listen(4000)
+server.listen(3000)
