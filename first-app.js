72eefcb702339e1252a2ba180e8cd6cdfe4e12d8
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-17 15:24:57 +0530

    clean

--- first-app.js
+++ first-app.js
@@ -1,37 +1,6 @@
 const http= require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url === '/'){
-    res.write('<html>');
-    res.write('<head><title>My First page</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></body>')
-    res.write('</html>');
-    return res.end();
-    }
-    else if(url === '/message' && method==='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        });
-       return req.on('end',()=>{
-            const parsedbody = Buffer.concat(body).toString();
-            const message =parsedbody.split('=')[1];
-            fs.writeFile('message.txt',message,(err)=>{
-                res.statusCode=302;
-                res.setHeader('Location','/')
-                return res.end();
-            }) 
-        })
-       
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First page</title></head>');
-    res.write('<body><h1>Hello from my Node.js page</h1></body>')
-    res.write('</html>');
-    res.end();
-    
-})
+
+const routs=require('./routes');
+
+const server=http.createServer(routs);
 server.listen(3000)
