98cb6ee82a90bdc2ce264749e7090f2e6deb3a47
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-18 12:12:04 +0530

    middlewares

--- first-app.js
+++ first-app.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("middleware");
+app.use('/',(req, res, next)=>{
+    console.log('This always runs');
     next();
 });
-
-app.use((req,res,next)=>{
+app.use('/addproduct',(req, res, next)=>{
+    console.log("next middleware")
+    res.send('<h1>Add product</h1>');
+});
+app.use('/',(req, res, next)=>{
     res.send('<h1>Hello from express</h1>');
     console.log("next middleware")
 });
