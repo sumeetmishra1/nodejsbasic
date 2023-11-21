c33210a5a4ee183f86cef64c1fdfc5083dc1a2e7
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-21 20:12:33 +0530

    serving html

--- first-app.js
+++ first-app.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const bodyparser= require('body-parser');
 const app = express();
+const path= require('path');
 const adminroutes= require('./Routes/admin');
 const shoproutes =require('./Routes/shop');
+const contactusroutes=require('./Routes/contactus');
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(shoproutes);
+app.use(contactusroutes);
 app.use('/admin',adminroutes);
+app.use(express.static(path.join(__dirname,'public')))
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
 })
